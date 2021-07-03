6b9511fcfcfcfa1ca80de91e3a4cfecc47320903
sanchezcris <sanchezcristopher48@gmail.com>
2021-07-03 13:40:46 -0600

    Se agregó metodo para graficar señal de entrada

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-#Librerías a utilizar:
+from wave import Wave_write
 import pyaudio
 import wave
 import matplotlib.pyplot as plt
@@ -6,9 +6,9 @@ import numpy as np
 import simpleaudio as sa
 import sounddevice as sd
 import tkinter as tk
-import os
+import scipy.io.wavfile as waves
+import pygame
 from IPython.display import Audio
-from scipy.io import wavfile
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
@@ -18,6 +18,7 @@ from tkinter import messagebox
 ventanaP = tk.Tk()
 ventanaP.title("Tratamiento de Señales - Grupo No. 1")
 ventanaP.geometry("1280x720")
+pygame.mixer.init()
 
 #Métodos y funciones:
 
@@ -26,10 +27,21 @@ def abrirArchivo():
     global archivo
     archivo = filedialog.askopenfilename(title = "abrir", initialdir="C:/Downloads", filetypes=(("sound files",".wav"),))
 
+def graficaSeñalEntrada():
+    muestreo, onda = waves.read(archivo)
+    longitud = np.shape(onda)
+    canal1 = onda[:, 0].copy()
+    plt.plot(canal1)
+    plt.xlabel('Tiempo')
+    plt.ylabel('Amplitud')
+    plt.show()
+
 def playFile():
-    #Leyendo el archivo wav
-    sample_rate, data = wavfile.read(archivo)
-    sd.play(data, 44100).wait()
+    pygame.mixer.music.load(archivo)
+    pygame.mixer.music.play(loops=0)
+
+def stopFile():
+    pygame.mixer.music.stop()
 
 def grabarMIC():
     duracion = 10           #el máximo de tiempo que permite grabar son 10 segundos
@@ -52,12 +64,28 @@ def grabarMIC():
     waveFile.writeframes(b''.join(frames))
     waveFile.close()
 
+def filtroPasaBajo():
+    s=grupo1.get()
+    if s==1: messagebox.showinfo(title="Diagnostico", message = "seleccionaste: Pasa BAjo")
+    if s==2: messagebox.showinfo(title="Diagnostico", message = "seleccionaste: Pasa Alto")
+    if s==3: messagebox.showinfo(title="Diagnostico", message = "seleccionaste: Pasa Banda")
+    
+
 #Botones:
 
 abrirbtn = Button(ventanaP, text = "Explorador de Archivos", command = abrirArchivo, activebackground="red", background="green").place(x=100, y=100)
 grabarbtn = Button(ventanaP, text = "Grabar con MIC", command = grabarMIC).place(x=300, y=100)
 textbtn = Button(ventanaP, text = "Texto a Sonido").place(x=600, y=100)
-playbtn1 = Button(ventanaP, text = "▶ Reproducir", command = playFile).place(x=100, y=150)
-playbtn2 = Button(ventanaP).place(x=400, y=150)
+playbtn1 = Button(ventanaP, text = "▶ Entrada 1", command = playFile).place(x=100, y=150)
+playbtn2 = Button(ventanaP, text = "▶ Entrada 2").place(x=300, y=150)
+playbtn3 = Button(ventanaP, text = "▶ Entrada 3").place(x=500, y=150)
+playbtn4 = Button(ventanaP, text = "▶ Salida 1").place(x=100, y=350)
+playbtn5 = Button(ventanaP, text = "▶ Salida 2").place(x=300, y=350)
+playbtn6 = Button(ventanaP, text = "▶ Salida 3").place(x=500, y=350)
+playbtn7 = Button(ventanaP, text = "▉ Stop", command = graficaSeñalEntrada).place(x = 800, y=350)
 
+grupo1 = IntVar() #variable para agrupar a los radio button
+rdbtn1 = Radiobutton(ventanaP, text = "Filtro 1", value=1, variable=grupo1, command= filtroPasaBajo).place(x=100, y = 200)
+rdbtn2 = Radiobutton(ventanaP, text = "Filtro 2", value=2, variable=grupo1, command= filtroPasaBajo).place(x=100, y = 250)
+rdbtn3 = Radiobutton(ventanaP, text = "Filtro 3", value=3, variable=grupo1, command= filtroPasaBajo).place(x=100, y = 300)
 ventanaP.mainloop() 
